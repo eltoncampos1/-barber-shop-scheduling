337e53b9cd05b75946d35585e8e495cf1a27ee30
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-19 03:46:55 -0300

    create authenticate middleware

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e6709a33820d8f737ad64c71618ac4d1',
+    expiresIn: '1d',
+  }
+}
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import User from '../models/User';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -32,9 +33,11 @@ class AuthenticateUserService {
       throw Error ('Incorrect email/password combination.')
     }
 
-    const token = sign({}, 'e6709a33820d8f737ad64c71618ac4d1', {
+    const {expiresIn,secret} = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
