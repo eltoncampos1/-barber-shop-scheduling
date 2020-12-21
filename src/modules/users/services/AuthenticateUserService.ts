e60a4c7e914827a204cb575633cc21d5542d2a9d
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-21 15:04:16 -0300

    resolve imports

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
-import User from '../infra/typeorm/entities/User';
+import User from '@modules/users/infra/typeorm/entities/User';
 import { sign } from 'jsonwebtoken';
 
-import AppError from '../../../shared/errors/AppError';
-import authConfig from '../../../config/auth';
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth';
 
 interface Request {
   email: string;
