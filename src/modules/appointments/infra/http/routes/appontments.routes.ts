c73b0d0d2c2854f82ab6c9384d37c835c262a6a8
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-22 21:30:07 -0300

    add controller to routes

--- src/modules/appointments/infra/http/routes/appontments.routes.ts
+++ src/modules/appointments/infra/http/routes/appontments.routes.ts
@@ -1,33 +1,13 @@
 import { Router } from 'express';
-import {  parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({date: parsedDate, provider_id})
-
-    response.json(appointment);
-
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
