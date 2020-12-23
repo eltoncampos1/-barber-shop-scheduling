c102c2dc5104d87afa962cc30b8c15b6def60e2d
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-23 04:30:38 -0300

    create a test for update avatar service

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,69 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeStorageprovider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakesUsersRepository from '../repositories/fakes/FakesUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to update avatar',async() => {
+    const fakeUsersRepository = new FakesUsersRepository();
+    const fakeStorageProvider = new FakeStorageprovider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name: 'john doe',
+      email: 'jon@example.com',
+      password: '123456',
+    })
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'avatar.jpg'
+    });
+
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should not be able to create a new avatar from a non-existing user',async() => {
+    const fakeUsersRepository = new FakesUsersRepository();
+    const fakeStorageProvider = new FakeStorageprovider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+
+    expect(updateUserAvatar.execute({
+      user_id: 'non-existing-user',
+      avatarFileName: 'avatar.jpg'
+    })).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete avatar when update new one',async() => {
+    const fakeUsersRepository = new FakesUsersRepository();
+    const fakeStorageProvider = new FakeStorageprovider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile')
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository,fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name: 'john doe',
+      email: 'jon@example.com',
+      password: '123456',
+    })
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'avatar.jpg'
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'avatar2.jpg'
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
+});
